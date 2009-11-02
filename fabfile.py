4394b276ed6fd27b77a3e76044f37cd861d9b194
Ben Cochran <bcochran@gmail.com>
2009-11-02 15:34:33 -0600

    Updated fabfile to support multiple environments and more robust deployment (including automatic cloning and setup if needed)

--- fabfile.py
+++ fabfile.py
@@ -1,20 +1,45 @@
 from __future__ import with_statement
-from fabric.api import run, env, local, cd
+from fabric.api import run, env, local, cd, require, abort
+from fabric.contrib.files import exists
 
-
-env.hosts = ['gnarus@slice.gnar.us']
-env.deploy_to = '/home/gnarus/slice'
 env.repository = 'git@github.com:bcochran/gnarly.git'
 env.branch = 'origin/master'
 
+## Environments
+
+def production():
+    env.hosts = ['gnarus@slice.gnar.us']
+    env.deploy_to = '/home/gnarus/production'
+
+def development():
+    env.hosts = ['gnarus@slice.gnar.us']
+    env.deploy_to = '/home/gnarus/development'
+
+# Actions
+
 def deploy():
-    update()
+    '''
+    Deploy the code. First push our repository, then either setup the
+    repository or update it.
+    '''
+    require('deploy_to')
+    
+    local("git push")
+    
+    if not exists(env.deploy_to):
+        setup()
+    else:
+        update()
     cleanup()
+
 
 def setup():
     '''
     Setup a fresh git-style deployment.
     '''
+    require('repository')
+    require('deploy_to')
+    
     run('git clone %(repository)s %(deploy_to)s' % env)
 
 
@@ -22,7 +47,6 @@ def update():
     '''
     Update the deployed code
     '''
-    
     with cd(env.deploy_to):
         run("git fetch origin; git reset --hard %(branch)s" % env)
 
@@ -31,10 +55,16 @@ def rollback():
     '''
     Rollback a single commit.
     '''
-    
+    if not exists(env.deploy_to):
+        abort("cannot rollback if we've never deployed")
     env.branch = "HEAD^"
-    deploy()    
+    update()    
 
 def cleanup():
-    # do we need this?
+    # do we need this to do anything?
     pass
+
+
+## Aliases
+prod = production
+dev = development
