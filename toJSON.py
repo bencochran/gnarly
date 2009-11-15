6595a5e4dc01ff4737a51860e64c8df7913d21a3
Wade Gobel <gobelw@carleton.edu>
2009-11-15 14:32:13 -0600

    Wade Gobel added a comment to the Python file.

--- toJSON.py
+++ toJSON.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
 '''
 Wade Gobel and Chris Peck
+A sample function that takes layer-relevant information,
+places it in a Python dictionary, and returns an appropriately
+formatted JSON string representing that dictionary.
 '''
 import json
 
